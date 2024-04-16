5711722824812135bdb8f42f1e6176c5ae4c5423
JaysonLuo <2628498451@qq.com>
2024-04-16 21:57:40 +0800

    使用unittest进行第一次测试

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,45 @@
 from selenium import webdriver
+import unittest
 
-# 创建 Chrome WebDriver 实例
-browser = webdriver.Chrome()
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        
+    def tearDown(self):
+        self.browser.quit()
 
-browser.get('http://localhost:8000')
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # 伊迪丝听说有一个很酷的在线待办事项应用
+        # 她去看了这个应用的首页
+        self.browser.get('http://localhost:8000')
 
-assert 'Django' in browser.page_source
+        # 她注意到网页的标题和头部都包含“待办事项”这个词
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # 应用邀请她输入一个待办事项
+        
+        # 她在一个文本框中输入了“Buy peacock feathers”（购买孔雀羽毛）
+
+        # 她按回车键后，页面更新了
+
+        # 待办事项表格中显示了“1: Buy peacock feathers”
+
+        # 页面中又显示了一个文本框，可以输入其他的待办事项
+
+        # 她输入了“Use peacock feathers to make a fly”（使用孔雀羽毛做假蝇）
+
+        # 页面再次更新，她的清单中显示了这两个待办事项
+
+        # 伊迪丝想知道这个网站是否会记住她的清单
+
+        # 她看到网站为她生成了一个唯一的 URL
+
+        # 而且页面中有一些文字解说这个功能
+
+        # 她访问那个 URL，发现她的待办事项列表还在
+
+        # 她很满意，去睡觉了
+
+if __name__ == '__main__':
+    unittest.main()
