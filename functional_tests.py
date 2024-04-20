b047f11a22b84eb262855053e72cb6f37b8a9a52
JaysonLuo <2628498451@qq.com>
2024-04-21 00:30:33 +0800

    功能测试现在check输入todo item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
+from selenium.webdriver.common.by import By
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -15,31 +18,28 @@ class NewVisitorTest(unittest.TestCase):
 
         # 她注意到网页的标题和头部都包含“待办事项”这个词
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
 
         # 应用邀请她输入一个待办事项
-        
-        # 她在一个文本框中输入了“Buy peacock feathers”（购买孔雀羽毛）
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # 她在一个文本框中输入了“Buy flowers”（购买鲜花）
+        inputbox.send_keys('Buy flowers')
 
         # 她按回车键后，页面更新了
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        # 待办事项表格中显示了“1: Buy peacock feathers”
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Buy flowers', [row.text for row in rows])
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
-
-        # 她输入了“Use peacock feathers to make a fly”（使用孔雀羽毛做假蝇）
-
-        # 页面再次更新，她的清单中显示了这两个待办事项
-
-        # 伊迪丝想知道这个网站是否会记住她的清单
-
-        # 她看到网站为她生成了一个唯一的 URL
-
-        # 而且页面中有一些文字解说这个功能
-
-        # 她访问那个 URL，发现她的待办事项列表还在
-
-        # 她很满意，去睡觉了
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main()
