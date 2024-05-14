4b415a6d5bf4649167fa403626e2e074e1f20b85
JaysonLuo <2628498451@qq.com>
2024-05-14 23:36:36 +0800

    message 11:show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -35,13 +35,37 @@ class HomePageTest(TestCase):
     #     found = resolve('/')
     #     self.assertEqual(found.func, home_page)
 
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+        response = self.client.get('/')
+        
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+        self.assertEqual(response.status_code, 302) 
+        self.assertEqual(response['location'], '/')
+
         self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
