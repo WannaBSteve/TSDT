3ea4ce24de93ce0118ddb43963775318a45fd06f
JaysonLuo <2628498451@qq.com>
2024-05-30 15:38:50 +0800

    修改req

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,69 @@
+from fabric.contrib.files import append, exists, sed
+from fabric.api import env, local, run
+import random
+
+REPO_URL = 'https://github.com/WannaBSteve/TSDT.git'
+CONDA_ENV_PATH = '/root/miniconda3/envs/TSDT'
+CONDA_ENV_NAME = 'TSDT'
+
+def deploy():
+    # site_foler = f'/home/{env.user}/sites/{env.host}'
+    site_foler = f'/root/sites/{env.host}'
+    source_folder = site_foler + '/source'
+    _create_directory_structure_if_necessary(site_foler)
+    _get_latest_source(source_folder)
+    _update_settings(source_folder, env.host)
+    # _update_virtualenv(source_folder)
+    _update_conda_env(source_folder)
+    _update_static_files(source_folder)
+    _update_database(source_folder)
+
+def _create_directory_structure_if_necessary(site_foler):
+    for subfolder in ('database', 'static', 'virtualenv', 'source'):
+        run(f'mkdir -p {site_foler}/{subfolder}')
+
+def _get_latest_source(source_folder):
+    if exists(source_folder + '/.git'):
+        run(f'cd {source_folder} && git fetch')
+    else:
+        run(f'git clone {REPO_URL} {source_folder}')
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    run(f'cd {source_folder} && git reset --hard {current_commit}')
+
+
+def _update_settings(source_folder, site_name):
+    settings_path = source_folder + '/notes/settings.py'
+    sed(settings_path, "DEBUG = True", "DEBUG = False")
+    sed(settings_path, 'ALLOWED_HOSTS =.+$', f'ALLOWED_HOSTS = ["{site_name}"]')
+    secret_key_file = source_folder + '/notes/secret_key.py'
+    if not exists(secret_key_file):
+        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+        key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
+        append(secret_key_file, f'SECRET_KEY = "{key}"')
+    append(settings_path, '\nfrom .secret_key import SECRET_KEY')   
+
+# def _update_virtualenv(source_folder):
+#     virtualenv_folder = source_folder + '/../virtualenv'
+#     if not exists(virtualenv_folder + '/bin/pip'):
+#         run(f'python3 -m venv {virtualenv_folder}')
+#     run(f'{virtualenv_folder}/bin/pip install -r {source_folder}/requirements.txt')
+
+def _update_conda_env(source_folder):
+    if not exists(f'/root/miniconda3/envs/{CONDA_ENV_NAME}'):
+        run(f'conda create -n {CONDA_ENV_NAME} python=3.9 -y')
+    run(f'/root/miniconda3/envs/{CONDA_ENV_NAME}/bin/pip install -r {source_folder}/requirement.txt')
+
+# def _update_static_files(source_folder):
+#     run(f'cd {source_folder} && ../virtualenv/bin/python manage.py collectstatic --noinput')
+
+def _update_static_files(source_folder):
+    run(f'cd {source_folder} && /root/miniconda3/envs/{CONDA_ENV_NAME}/bin/python manage.py collectstatic --noinput')
+
+# def _update_database(source_folder):
+#     run(f'cd {source_folder} && ../virtualenv/bin/python manage.py migrate --noinput')
+
+def _update_database(source_folder):
+    run(f'cd {source_folder} && /root/miniconda3/envs/{CONDA_ENV_NAME}/bin/python manage.py migrate --noinput')
+    
+if __name__ == '__main__':
+    local('fab -f /path/fabfile.py deploy:')
